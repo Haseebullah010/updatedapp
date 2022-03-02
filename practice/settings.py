8a9079466cbcacaab2fe19ce977117c351577707
Haseebullah010 <haseebtarar72@gmail.com>
2022-03-02 15:07:28 +0500

    "update"

--- practice/settings.py
+++ practice/settings.py
@@ -133,7 +133,8 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = 'static/'
-
+MEDIA_URL =  '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR,'public')
 STATICFILES_DIRS = [
     BASE_DIR / "public",
 ]
